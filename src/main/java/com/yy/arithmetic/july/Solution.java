64a6ee937c10064aa7a0a82c260c205a0d55d4f9
zane <jiaorenzhan828@163.com>
2020-07-24 16:12:02 +0800

    first

---
+++ src/main/java/com/yy/arithmetic/july/Solution.java
@@ -0,0 +1,12 @@
+package com.yy.arithmetic.july;
+
+/**
+ * @author Ivan yu
+ * @date 2020/07/24
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/yy/arithmetic/ArithmeticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yy.arithmetic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArithmeticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
